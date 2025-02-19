6f55aa71d2bdbdeef681a3d28f2368f89ebc5cf4
Sander G <sandergarita@gmail.com>
2025-02-19 10:38:23 -0600

    0.4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "388577817"
+    serviceWorkerVersion: "4181447770"
   }
 });
